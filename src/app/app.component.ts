0d613302aa7dd024cfae186a4426ada745920fa9
rahman-royalsoftit <rahman.royalsoftit@gmail.com>
2023-06-16 14:27:32 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-bootstrap-carousel';
+  title = 'Angular Carousel Example with Ng-Bootstrap';
+
+  imageUrl = "assets/images/";
+  
+  dataSource : SlideImage[]=[
+    {imageName : 'royalsoftit-01.gif', caption: 'School Management System', imageDescription:'School Management System'},
+    {imageName : 'royalsoftit-02.gif', caption: 'Accounts Management System', imageDescription:'Accounts Management System'},
+    {imageName : 'royalsoftit-03.gif', caption: 'HR Management System', imageDescription:'HR Management System'},
+    {imageName : 'royalsoftit-04.gif', caption: 'Payroll Management System', imageDescription:'Payroll Management System'},
+    
+  ];
+
+}
+
+
+export class SlideImage{
+  public imageName : string = '';
+  public caption : string = '';
+  public imageDescription : string = '';
 }
